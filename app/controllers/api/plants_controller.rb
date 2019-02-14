67299d8a2e2ac3add33a3fd0cac58a25f5e1abec
sunyounghwang <sunyounghwang13@gmail.com>
2019-02-14 14:58:00 -0500

    Create index and create actions and plant_params method

--- app/controllers/api/plants_controller.rb
+++ app/controllers/api/plants_controller.rb
@@ -1,3 +1,21 @@
 class API::PlantsController < ApplicationController
-  
+  def index
+    render json: Plant.all
+  end
+
+  def create
+    plant = Plant.new(plant_params)
+    
+    if plant.save
+      render json: plant
+    else
+      render json: { message: plant.errors }, status: 400
+    end
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:name, :type_of, :location, :start_date, :img_url, :notes)
+  end
 end
